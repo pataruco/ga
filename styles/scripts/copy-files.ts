650e136af8d0c7d2ff8b8d7ce93587a91417ee5e
Pedro Martin <pataruco@gmail.com>
2022-02-11 21:01:35 +0000

    Draft an style module

---
+++ styles/scripts/copy-files.ts
@@ -0,0 +1,33 @@
+import { extname } from 'path';
+import { readdir, cp } from 'fs/promises';
+import { resolve } from 'path';
+
+// inspired from https://stackoverflow.com/a/45130990/4842303
+export async function* getFilePaths(dir: string): AsyncGenerator<string> {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* getFilePaths(res);
+    } else {
+      yield res;
+    }
+  }
+}
+
+const isStyle = (file: string) =>
+  extname(file) === '.scss' || extname(file) === '.css';
+
+const toDistFolder = (file: string) => file.replace('/src', '/dist');
+
+const start = async () => {
+  for await (const file of getFilePaths('./src')) {
+    if (isStyle(file)) {
+      await cp(file, toDistFolder(file), { force: true, recursive: true });
+    }
+  }
+};
+
+start()
+  .then()
+  .catch((error) => console.error(error));
