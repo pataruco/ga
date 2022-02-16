5e29fb59e3181a5b7fd6fe9b97cfd16d794dca7f
Pedro Martin <pataruco@gmail.com>
2022-02-16 18:35:07 +0000

    Add lessons to seiEs

--- @shared/lessons/src/index.ts
+++ @shared/lessons/src/index.ts
@@ -1,5 +1,14 @@
 import { fewd } from './fewd';
 import { jsd } from './jsd';
+import { unit1 } from './sei-es/unit-1';
+import { unit2 } from './sei-es/unit-2';
+import { unit4 } from './sei-es/unit-4';
+
+const seiEs = {
+  unit1,
+  unit2,
+  unit4,
+};
 
 // Type definitions
 export type JSDCourse = typeof jsd;
@@ -8,4 +17,7 @@ export type JSDLesson = keyof JSDCourse;
 export type FEWDCourse = typeof fewd;
 export type FEWDLesson = keyof FEWDCourse;
 
-export { fewd, jsd };
+export type SEIESCourse = typeof unit1 | typeof unit2 | typeof unit4;
+export type SEIESLesson = keyof SEIESCourse;
+
+export { fewd, jsd, seiEs };
