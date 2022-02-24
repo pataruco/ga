b650a779519d09afc1ccaa00b55b0acd155d9883
Pedro Martin <pataruco@gmail.com>
2022-02-24 11:53:05 +0000

    Finish week 3

--- @shared/lessons/src/fewd-es/index.ts
+++ @shared/lessons/src/fewd-es/index.ts
@@ -4,12 +4,16 @@ import * as installfestMacos from './bonus-installfest-macos.md';
 import * as installfestWindows from './bonus-installfest-windows.md';
 import * as terminalGit from './03-terminal-git.md';
 import * as introToHtml from './04-intro-to-html.md';
+import * as introToCss from './05-intro-to-css.md';
+import * as boxModel from './06-box-model.md';
 
 export const fewdEs = {
   '01-welcome': welcome,
   '02-installfest': installfest,
   '03-terminal-git': terminalGit,
   '04-intro-to-html': introToHtml,
+  '05-intro-to-css': introToCss,
+  '06-box-model': boxModel,
   'bonus-installfest-macos': installfestMacos,
   'bonus-installfest-windows': installfestWindows,
 };
