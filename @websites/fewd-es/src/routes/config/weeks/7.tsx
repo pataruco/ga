53e5acc2550df0f1802b376b37d517d6b013304f
Pedro Martin <pataruco@gmail.com>
2022-03-04 16:54:47 +0000

    add sass and animation resources

--- @websites/fewd-es/src/routes/config/weeks/7.tsx
+++ @websites/fewd-es/src/routes/config/weeks/7.tsx
@@ -38,76 +38,40 @@ export const week7Route: RoutesByWeek = {
   //   name: 'Huddle',
   //   path: `/lessons/forms#17`,
   // },
-  // resources: [
-  //   [
-  //     'CSS Tricks: Font Size Idea: px at the Root, rem for Components, em for Text Elements ',
-  //     'https://css-tricks.com/rems-ems/',
-  //   ],
-  //   ['REM vs EM – The Great Debate', 'https://zellwk.com/blog/rem-vs-em/'],
-
-  //   [
-  //     'Treehouse: Beginners guide to responsive web design',
-  //     'http://blog.teamtreehouse.com/beginners-guide-to-responsive-web-design',
-  //   ],
-  //   [
-  //     'Smashing Magazine: Fixed vs Fluid layout (old article)',
-  //     'https://www.smashingmagazine.com/2009/06/fixed-vs-fluid-vs-elastic-layout-whats-the-right-one-for-you/',
-  //   ],
-  //   [
-  //     'A list apart: Responsive Web Design',
-  //     'https://alistapart.com/article/responsive-web-design',
-  //   ],
-  //   [
-  //     'A list apart: How to size text in CSS',
-  //     'http://alistapart.com/articles/howtosizetextincss',
-  //   ],
-  //   ['W3C: Media queries', 'https://www.w3.org/TR/css3-mediaqueries/'],
-  //   [
-  //     'Smashing Magazine: Responsive tools and techniques',
-  //     'https://www.smashingmagazine.com/2011/07/responsive-web-design-techniques-tools-and-design-strategies/',
-  //   ],
-  //   [
-  //     "Blast: 3 Reasons You Shouldn't Build a Mobile Website",
-  //     'http://www.blastam.com/blog/3-reasons-you-shouldnt-build-mobile-website',
-  //   ],
-  //   [
-  //     'Mobile friendly images',
-  //     'https://www.thesitewizard.com/css/responsive-images-for-mobile-friendly-website.shtml',
-  //   ],
-  //   [
-  //     'CSS Tricks: Retina Media Query',
-  //     'https://css-tricks.com/snippets/css/retina-display-media-query/',
-  //   ],
-  //   [
-  //     'CSS Tricks: Responsive Meta Tag',
-  //     'https://css-tricks.com/snippets/html/responsive-meta-tag/',
-  //   ],
-  //   [
-  //     'MDN: Input',
-  //     'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input',
-  //   ],
-  //   [
-  //     'HTML5 Doctor: Input Types',
-  //     'http://html5doctor.com/html5-forms-input-types/',
-  //   ],
-  //   ['Input Types', 'https://robertnyman.com/html5/forms/input-types.html'],
-  //   [
-  //     'MDN: Textarea',
-  //     'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/textarea',
-  //   ],
-  //   [
-  //     'CSS Tricks: Creating great web forms',
-  //     'https://css-tricks.com/tips-for-creating-great-web-forms/',
-  //   ],
-  //   [
-  //     'MDN: Sending form data',
-  //     'https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_and_retrieving_form_data',
-  //   ],
-  //   [
-  //     'HTML Goodies: So you want to build a form?',
-  //     'http://www.htmlgoodies.com/tutorials/forms/article.php/3479121',
-  //   ],
-  // ],
+  resources: [
+    ['Animation', 'https://css-tricks.com/almanac/properties/a/animation/'],
+    [
+      'CSS Animation for Beginners',
+      'https://robots.thoughtbot.com/css-animation-for-beginners',
+    ],
+    [
+      '15 Inspiring Examples of CSS Animation on CodePen',
+      'https://webdesign.tutsplus.com/articles/15-inspiring-examples-of-css-animation-on-codepen--cms-23937',
+    ],
+    [
+      'Transitions Docs',
+      'https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Transitions/Using_CSS_transitions',
+    ],
+    [
+      'Transition vs. Animation',
+      'https://cssanimation.rocks/transition-vs-animation/',
+    ],
+    [
+      'CSS Animation Support - caniuse.com',
+      'https://caniuse.com/#search=animation',
+    ],
+    ['SASS Basics', 'http://sass-lang.com/guide'],
+    [
+      'An Absolute Beginners Guide To SASS',
+      'http://blog.teamtreehouse.com/the-absolute-beginners-guide-to-sass',
+    ],
+    [
+      'Getting Started With SASS (with Interactive Examples)',
+      'https://scotch.io/tutorials/getting-started-with-sass',
+    ],
+    ['Advanced SCSS', 'https://gist.github.com/jareware/4738651'],
+    ['SASS Style Guide', 'https://css-tricks.com/sass-style-guide/'],
+  ],
   // bonuses: [
   //   [
   //     "Read through Shay Howe's Responsive Web Design",
