328e1f1cf0a3c487c504998e1e589f6e3a239d16
Pedro Martin <pataruco@gmail.com>
2022-03-06 17:40:44 +0000

    Finish bonus lessons

--- @shared/lessons/src/fewd-es/index.ts
+++ @shared/lessons/src/fewd-es/index.ts
@@ -1,7 +1,5 @@
 import * as welcome from './01-welcome.md';
 import * as installfest from './02-installfest.md';
-import * as installfestMacos from './bonus-installfest-macos.md';
-import * as installfestWindows from './bonus-installfest-windows.md';
 import * as terminalGit from './03-terminal-git.md';
 import * as introToHtml from './04-intro-to-html.md';
 import * as introToCss from './05-intro-to-css.md';
@@ -13,9 +11,15 @@ import * as positioning from './10-positioning.md';
 import * as responsive from './11-responsive-web-development.md';
 import * as forms from './12-forms.md';
 import * as animations from './13-animations.md';
-import * as introToJs from './15-intro-to-js.md';
 import * as sassSvg from './14-sass-svg.md';
+import * as introToJs from './15-intro-to-js.md';
 import * as controlFlow from './16-control-flow.md';
+// Bonus
+import * as a11y from './bonus-a11y.md';
+import * as ecmascript from './bonus-ecmascript.md';
+import * as installfestMacos from './bonus-installfest-macos.md';
+import * as installfestWindows from './bonus-installfest-windows.md';
+import * as tables from './bonus-tables.md';
 
 export const fewdEs = {
   '01-welcome': welcome,
@@ -34,6 +38,9 @@ export const fewdEs = {
   '14-sass-svg': sassSvg,
   '15-intro-to-js': introToJs,
   '16-control-flow': controlFlow,
+  'bonus-a11y': a11y,
+  'bonus-ecmascrip': ecmascript,
   'bonus-installfest-macos': installfestMacos,
   'bonus-installfest-windows': installfestWindows,
+  'bonus-tables': tables,
 };
