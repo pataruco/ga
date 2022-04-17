f164fa2ff4b2ac58e80ab11ac6bc81f646c1fe6d
Pedro Martin <pataruco@users.noreply.github.com>
2022-04-17 16:40:05 +0100

    Add analytics to fewd-es (#52)

--- @websites/fewd-es/src/routes/index.tsx
+++ @websites/fewd-es/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import Home from '../pages/home';
 import ErrorPage from '../pages/404';
 import About from '../pages/about';
@@ -10,22 +11,31 @@ import {
 } from './config';
 
 import { BonusLessonsRoute } from './config/bonus-lessons';
+import { useEffect } from 'react';
 
 const { path: bonusLessonsRoute, element: BonusLessons } = BonusLessonsRoute;
 
-const Router = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/lessons" element={<LessonsPage />} />
-      <Route path={bonusLessonsRoute} element={<BonusLessons />} />
-      {WeekRouteComponents}
-      {LessonRouteComponents}
-      {BonusLessonsRouteComponents}
-      <Route path="/*" element={<ErrorPage />} />
-    </Routes>
-  </BrowserRouter>
-);
+export const GA_TRACK_ID = 'G-48TLZ9CMME';
+
+const Router = () => {
+  useEffect(() => {
+    ReactGA.initialize(GA_TRACK_ID);
+  });
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/lessons" element={<LessonsPage />} />
+        <Route path={bonusLessonsRoute} element={<BonusLessons />} />
+        {WeekRouteComponents}
+        {LessonRouteComponents}
+        {BonusLessonsRouteComponents}
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Router;
