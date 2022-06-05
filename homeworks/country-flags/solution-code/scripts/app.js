2fe98b51bfa551419e8f477de0d74e8fafec743c
Pedro Martin <pataruco@gmail.com>
2022-06-05 09:15:57 +0100

    Refactor country flags hw solution

--- homeworks/country-flags/solution-code/scripts/app.js
+++ homeworks/country-flags/solution-code/scripts/app.js
@@ -1,57 +1,59 @@
-function init() {
-  const countriesContainer = document.querySelector('.countries');
-  const selectInput = document.querySelector('select');
-  const textInput = document.querySelector('input');
-  let countries = [];
-  let region = 'All';
-  let search = '';
+const countriesContainer = document.querySelector('.countries');
+const selectInput = document.querySelector('select');
+const textInput = document.querySelector('input');
 
-  async function getCountries() {
-    try {
-      const response = await fetch('https://restcountries.eu/rest/v2/all');
-      countries = await response.json();
-      displayCountries();
-    } catch (err) {
-      console.log(err);
-    }
-  }
+let countries = [];
+let region = 'All';
+let search = '';
 
-  function filterCountries() {
-    const re = new RegExp(search, 'i');
-    return countries.filter((country) => {
-      return (
-        re.test(country.name) && (country.region === region || region === 'All')
-      );
-    });
-  }
-
-  function displayCountries() {
-    const countriesHTMLArray = filterCountries().map((country) => {
-      const { name, nativeName, flag } = country;
-      return `
-        <div>
-          <h4>${name}</h4>
-          <h6>${nativeName}</h6>
-          <img src="${flag}" alt="${name} flag" />
-        </div>
-    `;
-    });
-    countriesContainer.innerHTML = countriesHTMLArray.join('');
-  }
-
-  function handleSelectInput(e) {
-    region = e.target.value;
+async function getCountries() {
+  try {
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    countries = await response.json();
     displayCountries();
+  } catch (error) {
+    console.error(error);
   }
-
-  function handleTextInput(e) {
-    search = e.target.value;
-    displayCountries();
-  }
-
-  getCountries();
-  selectInput.addEventListener('change', handleSelectInput);
-  textInput.addEventListener('keyup', handleTextInput);
 }
 
-window.addEventListener('DOMContentLoaded', init);
+function filterCountries() {
+  const searchString = new RegExp(search, 'i');
+  return countries.filter((country) => {
+    return (
+      searchString.test(country.name.common) &&
+      (country.region === region || region === 'All')
+    );
+  });
+}
+
+function displayCountries() {
+  const countriesHTMLArray = filterCountries().map((country) => {
+    const {
+      name: { common },
+      flags: { svg },
+    } = country;
+    return `
+        <section>
+          <h2>${common}</h4>
+          <img src="${svg}" alt="${common} flag" />
+        </section>
+    `;
+  });
+
+  countriesContainer.innerHTML = countriesHTMLArray.join('');
+}
+
+function handleSelectInput(event) {
+  region = event.target.value;
+  displayCountries();
+}
+
+function handleTextInput(e) {
+  search = event.target.value;
+  displayCountries();
+}
+
+selectInput.addEventListener('change', handleSelectInput);
+textInput.addEventListener('keyup', handleTextInput);
+
+window.addEventListener('DOMContentLoaded', getCountries);
