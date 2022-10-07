871595f1422a03d255ad4b3bf13f987bb9c5441c
Pedro Martin <pataruco@gmail.com>
2022-10-07 14:26:39 +0100

    Add week 4

--- @websites/fewd/src/routes/config/index.tsx
+++ @websites/fewd/src/routes/config/index.tsx
@@ -5,7 +5,7 @@ import { RoutesByWeek } from '../../@types/routes';
 import { week1Route } from './weeks/1';
 import { week2Route } from './weeks/2';
 import { week3Route } from './weeks/3';
-// import { week4Route } from './weeks/4';
+import { week4Route } from './weeks/4';
 // import { week5Route } from './weeks/5';
 // import { week6Route } from './weeks/6';
 // import { week7Route } from './weeks/7';
@@ -46,7 +46,7 @@ export const routesByWeek: RoutesByWeek[] = [
   week1Route,
   week2Route,
   week3Route,
-  // week4Route,
+  week4Route,
   // week5Route,
   // week6Route,
   // week7Route,
