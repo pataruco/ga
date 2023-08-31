9e272247352d6e36fc46452b58ebd6271499e1f6
Pedro Martin <pataruco@users.noreply.github.com>
2023-08-31 15:11:49 +0100

    NX migration (#108)
    * Remove monorepo tools
    
    * Add nx barebones
    
    * Manual migrate styles
    
    * Add components without building
    
    * Create a next website
    
    * Create a next website

---
+++ components/src/index.ts
@@ -0,0 +1,26 @@
+import Footer from './footer';
+// Icons
+import GALogoTextWhite from './icons/ga-text-white';
+import GALogoWhite from './icons/ga-white';
+import GitHubLogo from './icons/github';
+import LinkedinLogo from './icons/linkedin';
+import GaLogo from './icons/logo';
+import TwitterLogo from './icons/twitter';
+
+// Slides deck
+import SlidesDeck from './slides-deck';
+
+// Title
+import Title from './title';
+
+export {
+  Footer,
+  GALogoTextWhite,
+  GALogoWhite,
+  GaLogo,
+  GitHubLogo,
+  LinkedinLogo,
+  SlidesDeck,
+  Title,
+  TwitterLogo,
+};
