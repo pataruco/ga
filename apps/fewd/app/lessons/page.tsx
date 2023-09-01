a42c065c3c195ee3ade44f3440451958fd36e91b
Pedro Martin <pataruco@gmail.com>
2023-09-01 13:23:41 +0100

    Abstract lessons

--- apps/fewd/app/lessons/page.tsx
+++ apps/fewd/app/lessons/page.tsx
@@ -2,6 +2,8 @@
 
 import { Footer } from '@ga/components';
 import { SiteStyles } from '@ga/styles';
+import { lessons } from '../../curriculum/lessons';
+
 import Link from 'next/link';
 import 'normalize.css';
 SiteStyles;
@@ -18,49 +20,13 @@ const StyledPage = styled.div`
 
   main {
     flex: 1 auto;
+    padding: 1.25rem;
+  }
+
+  li {
+    margin-bottom: 0.75rem;
   }
 `;
-
-const lessons = [
-  {
-    content: 'Orientation and introduction to HTML',
-    link: '01-html-basics',
-  },
-  {
-    content: 'Advanced HTML & Intro to CSS',
-    link: '02-advanced-html-intro-to-css',
-  },
-  { content: 'Box model', link: '03-box-model' },
-  {
-    content: 'CSS Selectors, Specificity & Floats',
-    link: '04-css-selectors-specificity-float',
-  },
-  { content: 'Flexbox', link: '05-flexbox' },
-  { content: 'Positioning', link: '06-positioning' },
-  { content: 'Responsive Web development', link: '07-responsive' },
-  {
-    content: 'Responsive Web development lab 🧪',
-    link: '08-responsive-lab',
-  },
-  { content: 'Intro to JavaScript', link: '09-intro-to-js' },
-  {
-    content: 'Document Object Model (DOM)',
-    link: '10-document-object-model',
-  },
-  { content: 'Conditional statements', link: '11-conditional-statements' },
-  { content: 'Arrays and loops', link: '12-arrays-loops' },
-  { content: 'Animations, transitions, effects', link: '13-animations' },
-  { content: 'JavaScript plugins', link: '14-plugins' },
-  { content: 'Forms', link: '15-forms' },
-  { content: 'API Requests and Responses', link: '16-api' },
-  { content: 'Git & Sass', link: '17-git-sass' },
-  { content: 'SVG and Accessibility', link: '18-a11y-svg' },
-  {
-    content: 'User interface libraries and final presentations lab 🧪',
-    link: '19-bootstrap',
-  },
-  { content: 'Final presentations 🚀', link: '20-final-presentations' },
-];
 
 export default async function Index() {
   return (
@@ -69,9 +35,11 @@ export default async function Index() {
       <main>
         <h1>Lessons</h1>
         <ol>
-          {lessons.map(({ link, content }) => (
+          {lessons.map(({ link, content }, i) => (
             <li>
-              <Link href={`/lessons/${link}`}>{content}</Link>
+              <Link href={`/lessons/${link}`} key={i}>
+                {content}
+              </Link>
             </li>
           ))}
         </ol>
---
+++ apps/fewd/curriculum/lessons.ts
@@ -0,0 +1,40 @@
+export const lessons = [
+  {
+    content: 'Orientation and introduction to HTML',
+    link: '01-html-basics',
+  },
+  {
+    content: 'Advanced HTML & Intro to CSS',
+    link: '02-advanced-html-intro-to-css',
+  },
+  { content: 'Box model', link: '03-box-model' },
+  {
+    content: 'CSS Selectors, Specificity & Floats',
+    link: '04-css-selectors-specificity-float',
+  },
+  { content: 'Flexbox', link: '05-flexbox' },
+  { content: 'Positioning', link: '06-positioning' },
+  { content: 'Responsive Web development', link: '07-responsive' },
+  {
+    content: 'Responsive Web development lab 🧪',
+    link: '08-responsive-lab',
+  },
+  { content: 'Intro to JavaScript', link: '09-intro-to-js' },
+  {
+    content: 'Document Object Model (DOM)',
+    link: '10-document-object-model',
+  },
+  { content: 'Conditional statements', link: '11-conditional-statements' },
+  { content: 'Arrays and loops', link: '12-arrays-loops' },
+  { content: 'Animations, transitions, effects', link: '13-animations' },
+  { content: 'JavaScript plugins', link: '14-plugins' },
+  { content: 'Forms', link: '15-forms' },
+  { content: 'API Requests and Responses', link: '16-api' },
+  { content: 'Git & Sass', link: '17-git-sass' },
+  { content: 'SVG and Accessibility', link: '18-a11y-svg' },
+  {
+    content: 'User interface libraries and final presentations lab 🧪',
+    link: '19-bootstrap',
+  },
+  { content: 'Final presentations 🚀', link: '20-final-presentations' },
+];
