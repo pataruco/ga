b4a3233a48b3c6e6b91a8c7b043f4d4f703aec6f
Pedro Martin <pataruco@gmail.com>
2023-09-01 15:52:54 +0100

    Add week 2

--- apps/fewd/curriculum/homeworks.ts
+++ apps/fewd/curriculum/homeworks.ts
@@ -1,18 +1,18 @@
 export const homeworks = [
   {
     content: 'Fictional CV',
-    link: '/lessons/advanced-html-intro-to-css#43',
+    link: '/lessons/02-advanced-html-intro-to-css#44',
   },
   {
     content: 'Relaxr',
-    link: '/lessons/css-selectors-specificity-float#22',
+    link: '/lessons/04-css-selectors-specificity-float#22',
   },
   {
     content: 'Relaxr (multi column layout)',
-    link: '/lessons/positioning#27',
+    link: '/lessons/06-positioning#27',
   },
   {
     content: 'Responsive Web development',
-    link: '/lessons/responsive-lab#3',
+    link: '/lessons/08-responsive-lab#3',
   },
 ];
