121a69084258af1cb5b33df7ff9bebb4c8c57e4c
Pedro Martin <pataruco@gmail.com>
2023-09-01 16:23:13 +0100

    Add week 6 & 7

--- apps/fewd/libs/get-lessons-homeworks-links.ts
+++ apps/fewd/libs/get-lessons-homeworks-links.ts
@@ -1,8 +1,13 @@
 import { homeworks } from '../curriculum/homeworks';
 import { lessons } from '../curriculum/lessons';
 
-export const getLessonsHomeworksLinks = (weekNumber: number) => ({
-  lesson1: lessons[weekNumber - 1],
-  lesson2: lessons[weekNumber],
-  homework: homeworks[weekNumber - 1] ?? null,
-});
+export const getLessonsHomeworksLinks = (weekNumber: number) => {
+  // FIXME: this is a hack to make the first week work
+  const index = weekNumber === 1 ? weekNumber - 1 : weekNumber;
+
+  return {
+    lesson1: lessons[index],
+    lesson2: lessons[index + 1],
+    homework: homeworks[index] ?? null,
+  };
+};
