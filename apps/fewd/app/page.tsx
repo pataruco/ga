db5c4922322356d397c17e410c17f20be416659e
Pedro Martin <pataruco@gmail.com>
2023-09-01 19:36:49 +0100

    Render home table

--- apps/fewd/app/page.tsx
+++ apps/fewd/app/page.tsx
@@ -5,7 +5,33 @@ import styled from 'styled-components';
 
 import { Footer } from '@ga/components';
 import { SiteStyles } from '@ga/styles';
+
 import Header from '../components/header';
+
+import ResourcesPerWeekTableRow from '../components/resources-per-week-table-row';
+import week1 from '../curriculum/weeks/1';
+import week10 from '../curriculum/weeks/10';
+import week2 from '../curriculum/weeks/2';
+import week3 from '../curriculum/weeks/3';
+import week4 from '../curriculum/weeks/4';
+import week5 from '../curriculum/weeks/5';
+import week6 from '../curriculum/weeks/6';
+import week7 from '../curriculum/weeks/7';
+import week8 from '../curriculum/weeks/8';
+import week9 from '../curriculum/weeks/9';
+
+const routesByWeek = [
+  week1,
+  week2,
+  week3,
+  week4,
+  week5,
+  week6,
+  week7,
+  week8,
+  week9,
+  week10,
+];
 
 SiteStyles;
 
@@ -39,9 +65,9 @@ export default async function Index() {
           <tr>{TableHeaders}</tr>
         </thead>
         <tbody>
-          {/* {routesByWeek.map((routeByWeek, i) => (
+          {routesByWeek.map((routeByWeek, i) => (
             <ResourcesPerWeekTableRow {...routeByWeek} key={i} />
-          ))} */}
+          ))}
         </tbody>
       </table>
 
--- apps/fewd/components/resources-per-week-table-row.tsx
+++ apps/fewd/components/resources-per-week-table-row.tsx
@@ -1,26 +1,25 @@
 import Link from 'next/link';
 import React from 'react';
-import type { RoutesByWeek } from '../@types/routes';
 
-const ResourcesPerWeekTableRow: React.FC<RoutesByWeek> = ({
+const ResourcesPerWeekTableRow: React.FC<Week> = ({
   weekNumber,
-  lesson1: { name: name1, path: path1 },
-  lesson2: { name: name2, path: path2 },
+  lesson1,
+  lesson2,
   homework,
 }) => {
   return (
     <tr>
       <td>
-        <Link href={`week-${weekNumber}`}>{weekNumber}</Link>
+        <Link href={`week/${weekNumber}`}>{weekNumber}</Link>
       </td>
       <td>
-        <Link href={path1}>{name1}</Link>
+        <Link href={lesson1.link}>{lesson1.content}</Link>
       </td>
       <td>
-        <Link href={path2}>{name2}</Link>
+        <Link href={lesson2.link}>{lesson2.content}</Link>
       </td>
       <td>
-        {homework ? <Link href={homework.path}>{homework.name}</Link> : ''}
+        {homework ? <Link href={homework.link}>{homework.content}</Link> : ''}
       </td>
     </tr>
   );
