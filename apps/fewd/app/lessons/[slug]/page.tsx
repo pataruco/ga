991e3082d2d3ba78bfc2eec447daece0e3d05f54
Pedro Martin <pataruco@gmail.com>
2023-09-01 11:53:34 +0100

    Fallback to lessons page when failed to load slides

--- apps/fewd/app/lessons/[slug]/page.tsx
+++ apps/fewd/app/lessons/[slug]/page.tsx
@@ -3,6 +3,8 @@
 import Script from 'next/script';
 
 import { SlideStyles } from '@ga/styles';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { instantiateSlides } from '../../../libs/instantiate-slides';
 SlideStyles;
 
@@ -13,14 +15,23 @@ interface LessonPageProps {
 }
 
 const Page: React.FC<LessonPageProps> = ({ params: { slug } }) => {
+  const router = useRouter();
+  const slideUrl = `https://pataruco.s3.amazonaws.com/ga/lessons/fewd/${slug}.md`;
+
+  useEffect(() => {
+    fetch(slideUrl).then((response) => {
+      if (response.status !== 200) {
+        router.push('/lessons');
+      }
+    });
+  }, [slideUrl]);
+
   return (
     <>
       <Script
         src="https://remarkjs.com/downloads/remark-latest.min.js"
         onLoad={() => {
-          instantiateSlides(
-            `https://pataruco.s3.amazonaws.com/ga/lessons/fewd/${slug}.md`
-          );
+          instantiateSlides(slideUrl);
         }}
       />
     </>
