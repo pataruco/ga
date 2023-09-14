73c5f0bba04b0d48286f114f30c8af6f333f3f61
Pedro Martin <pataruco@gmail.com>
2023-09-14 14:40:01 +0100

    Set main layout in about page

---
+++ apps/fewd/app/about/loading.tsx
@@ -0,0 +1,9 @@
+import MainLayout from 'apps/fewd/components/main-layout';
+
+export default function Loading() {
+  return (
+    <MainLayout>
+      <h1>Loading ...</h1>
+    </MainLayout>
+  );
+}
