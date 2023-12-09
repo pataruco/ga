388440d9b8976c84d215168d2487d29b51336351
Pedro Martin <pataruco@gmail.com>
2023-12-09 08:54:36 +0000

    Add Firebase creds to .env

--- apps/fewd/libs/db.ts
+++ apps/fewd/libs/db.ts
@@ -2,14 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBmmleCZoV6i_gDAG5hWydyBoAVcmA_U40',
-  authDomain: 'all-things-ga.firebaseapp.com',
-  databaseURL:
-    'https://all-things-ga-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'all-things-ga',
-  storageBucket: 'all-things-ga.appspot.com',
-  messagingSenderId: '406008504083',
-  appId: '1:406008504083:web:3740d3fb97d7a1d20a04e6',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
